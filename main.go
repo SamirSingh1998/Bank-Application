ec98aa9ca8b77681f7f5e12501abca136d4fb110
Samir Singh <45367279+SamirSingh1998@users.noreply.github.com>
2023-07-01 18:54:33 +0530

    add route and handlers to greet and getAllCustomerData

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json: "full_name" xml: "full_name"` //if we dont use these tags then response will be like [{"Name":"Ashish","City":"Ranchi","ZipCode":"834009"},{"Name":"Ramesh","City":"Bangalore","ZipCode":"560017"}]
+	City    string `json: "city" xml: "city"`           //which is in capital letters
+	ZipCode string `json: "zip_code" xml: "zip_code"`
+}
+
+func main() {
+
+	//define routes
+	http.HandleFunc("/greet", greet)
+
+	http.HandleFunc("/customers", getAllCustomer)
+
+	//starting server
+	//Listen and Serve starts the server at localhost:port and second parameter is handler which is a request multiplexer, and since we are relying on default so we will keep it as nil.
+	//http.ListenAndServe("localhost:8000", nil) //To ensure that your program runs continuously, you can run the http.ListenAndServe function call in a separate goroutine.
+	go func() {
+		if err := http.ListenAndServe("localhost:8000", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	log.Println("Server is up and running on localhost:8000")
+
+	// after running main. go
+	// curl http://localhost:9000/greet
+	//	Hello World
+
+	select {} // Use a blocking mechanism to keep the program running
+}
+
+// in handler func we have two params, response writer that writes back to client and second is  request reader that takes request from client
+// we will use Fprint func to send the response to the client, this takes two params: one is IO writer and other is data that we want to send to IO writer
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!") //simple handler func that returns "Hello World!" as response in http ResponseWriter
+}
+
+func getAllCustomer(w http.ResponseWriter, r *http.Request) { // Handler func to list all customers
+	customer := []Customer{
+		{"Ashish", "Ranchi", "834009"},
+		{"Ramesh", "Bangalore", "560017"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" { //if request Header contains Content-Type as xml then
+		w.Header().Add("Content-Type", "application/xml") // we want to give output as xml
+		xml.NewEncoder(w).Encode(customer)
+	} else { //else we will give response as json
+		w.Header().Add("Content-Type", "application/json") // this is to recognize the response content type as json,
+		//without this the response will be of type json only since we have used json encoder below but wouldn't be viewed as json,
+		//instead will be viewed as raw text in postman response
+
+		json.NewEncoder(w).Encode(customer) //encode the list to json format
+	}
+}
